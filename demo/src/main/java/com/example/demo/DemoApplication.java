fa157656d96f4797bb1c9b5b4512545290653e1f
Matt <74383191+m-c-hill@users.noreply.github.com>
2023-08-16 12:07:18 +0100

    Chapter 4: Database Access (#2)
    * add h2 and spring data to pom
    
    * update coffee controller to use database

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,9 +26,14 @@ public class DemoApplication {
 
 }
 
+@Entity
 class Coffee {
-	private final String id;
+	@Id
+	private String id;
 	private String name;
+
+	public Coffee(){
+	}
 
 	public Coffee(String id, String name) {
 		this.id = id;
@@ -41,64 +52,72 @@ class Coffee {
 		return name;
 	}
 
+	public void setId(String id){
+		this.id = id;
+	}
+
 	public void setName(String name){
 		this.name = name;
 	}
 }
 
+// Wraps around the object type to store in the db and the type of its unique ID, in this case string
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
+
+
 @RestController
 @RequestMapping("/coffees")
 class RestApiDemoController {
-	private List<Coffee> coffees = new ArrayList<>();
+	private final CoffeeRepository coffeeRepository;
 
-	public RestApiDemoController() {
-		coffees.addAll(List.of(
+	public RestApiDemoController(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
+	}
+
+	@GetMapping
+	Iterable<Coffee> getCoffees() {
+		return coffeeRepository.findAll();
+	}
+
+	@GetMapping(value="/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		return coffeeRepository.findById(id);
+	}
+
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee){
+		return coffeeRepository.save(coffee);
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+		return coffeeRepository.existsById(id) ?
+				new ResponseEntity<>(coffee, HttpStatus.OK) :
+				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED);
+	};
+
+	@DeleteMapping("/{id}")
+	void deleteCoffee(@PathVariable String id){
+		coffeeRepository.deleteById(id);
+	}
+}
+
+@Component
+class DataLoader {
+	private final CoffeeRepository coffeeRepository;
+
+	public DataLoader(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
+	}
+
+	@PostConstruct
+	private void loadData() {
+		coffeeRepository.saveAll(List.of(
 				new Coffee("Cafe Cereza"),
 				new Coffee("Cafe Ganador"),
 				new Coffee("Cafe Lareno"),
 				new Coffee("Cafe Tres Pontas")
 		));
 	}
-
-	@GetMapping
-	Iterable<Coffee> getCoffees() {
-		return coffees;
-	}
-
-	@GetMapping(value="/{id}")
-	Optional<Coffee> getCoffeeById(@PathVariable String id) {
-		for (Coffee c: coffees) {
-			if (c.getId().equals(id)) {
-				return Optional.of(c);
-			}
-		}
-		return Optional.empty();
-	}
-
-	@PostMapping
-	Coffee postCoffee(@RequestBody Coffee coffee){
-		coffees.add(coffee);
-		return coffee;
-	}
-
-	@PutMapping("/{id}")
-	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-		int coffeeIndex = -1;
-
-		for (Coffee c: coffees) {
-			if (c.getId().equals(id)) {
-				coffeeIndex = coffees.indexOf(c);
-				coffees.set(coffeeIndex, coffee);
-			}
-		}
-		return (coffeeIndex == -1) ?
-				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
-				new ResponseEntity<>(coffee, HttpStatus.OK);
-	};
-
-	@DeleteMapping("/{id}")
-	void deleteCoffee(@PathVariable String id){
-		coffees.removeIf(c -> c.getId().equals(id));
-	}
-
 }
+
